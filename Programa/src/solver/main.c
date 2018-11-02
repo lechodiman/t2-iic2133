9073d82bf4130a24ef3a81b39b21086c1b380d9f
lechodiman <lechodiman@uc.cl>
2018-11-02 10:06:13 -0300

    read tests files

--- Programa/src/solver/main.c
+++ Programa/src/solver/main.c
@@ -15,16 +15,43 @@ int main(int argc, char *argv[])
         printf("\t[output.txt] es el nombre del archivo a escribir con el output\n");
     }
 
-    Queue* q = queue_init();
-    queue_add(q, "123456780");
-    queue_add(q, "012345678");
-    char output[17] = "";
-    queue_remove(q, output);
-    printf("output: %s\n", output);
-    queue_remove(q, output);
-    printf("output: %s\n", output);
+    FILE* test_file = fopen(argv[1], "r");
+    FILE* output = fopen(argv[2], "w");
 
-    printf("is empty: %d\n", queue_is_empty(q));
-    queue_free(q);
+    if (!test_file || !output)
+    {
+        printf("Error abriendo archivos!\n");
+        return 2;
+    }
+
+    int dimension;
+    char string_board[17] = "";
+
+    fscanf(test_file, "%d\n", &dimension);
+
+    printf("DIMENSION: %d\n", dimension);
+
+    int i = 0;
+    for (int row = 0; row < dimension; ++row)
+    {
+        for (int col = 0; col < dimension; ++col)
+        {
+            int number;
+            fscanf(test_file, "%d,\n", &number);
+            string_board[i] = number + '0';
+            i++;
+        }
+    }
+
+    printf("%s\n", string_board);
+
+    Board* board = board_init(dimension);
+    board_to_matrix(board, string_board);
+    board_print(board);
+
+    board_free(board);
+    fclose(test_file);
+    fclose(output);
+
     return 0;
 }
